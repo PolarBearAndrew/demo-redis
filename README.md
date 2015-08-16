d027f7621351eeef2d40ef2b89fb46e638c2237a
Andrew <chenpoanandrew@gmail.com>
2015-08-16 10:25:34 +0800

    add doc

--- README.md
+++ README.md
@@ -6,22 +6,73 @@ demo redis cache
 * [Redis - npm](https://www.npmjs.com/package/redis)
 * [Redis - GUI](http://redisdesktop.com)
 
+## 安裝
+
 先從官方網站下載 Redis，解開壓縮檔之後，執行
 
-```
+``` bat
 $ make 
 ```
 
 Redis 會自行安裝，需要一段時間
 
-```
+``` bat
 $ make test
 ```
 
 Redis 會自行進行測試，測時完成後即可啟動，啟動並不需要在指定的資料夾下，屬於全域的
 
-```
+``` bat
 $ redis-server
 ```
 
-接著在 GUI 的介面中進行連線即可
+接著在 GUI 的介面中進行連線就可以檢視現在 Redis 內的資料。
+
+
+## 使用
+
+在 Node.js 引用 Redis 的套件並且連線
+
+``` js
+var redis = require("redis").createClient('6379', '127.0.0.1'); //port, IP
+```
+
+redis.set 寫入值
+
+``` js
+redis.set( key, value, function( err, reply ){
+  console.log( reply.toString() ); // 新增成功會回傳 ok
+});
+```
+
+redis.get 取得值
+
+``` js
+redis.get( key, function( err, reply ){
+  console.log( reply.toString() ); // 新增會回傳 value
+});
+```
+
+cache 資料的存活時間
+
+``` js
+redis.expire( key, 60);
+```
+
+redis 錯誤 callback function
+
+``` js
+redis.on( "connect", callback );
+```
+
+redis 錯誤 callback function
+
+``` js
+redis.on( "error", callback );
+```
+
+redis 驗證
+
+``` js
+redis.auth("AndrewChen");
+```
--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -1,30 +1,42 @@
 var express = require('express');
 var router  = express.Router();
-var redis   = require("redis").createClient(); //redis
+var redis   = require("redis").createClient('6379', '127.0.0.1'); //redis
 
-
+//redis 錯誤訊息
 redis.on("error", function (err) {
     console.log("Error " + err);
 });
 
 //連線時觸發
 //redis.on("connect", runSample);
+//
+//redis.auth("AndrewChen"); //驗證
 
 router.get('/set', function(req, res, next) {
 
   //寫入資料
   redis.set("demoRedis", req.query.value, function (err, reply) {
+
+    //設定存活時間 60s = 1 min
+    redis.expire('demoRedis', 60);
+
+    //回應頁面
     res.render('index', { title: '寫入資料:' + reply.toString() });
   });
 });
 
 router.get('/get', function(req, res, next) {
 
-  var value = req.query.value;
-
   //取得資料
   redis.get("demoRedis", function (err, reply) {
-      res.render('index', { title: '取得資料:' + reply.toString() });
+
+    //有人讀取時，刷新存活時間
+    redis.expire('demoRedis', 60);
+
+    //回應頁面
+    var value = reply.toString() || '無快取資料';
+
+    res.render('index', { title: '取得資料:' + value });
   });
 });
 
